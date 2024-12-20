35254904dfaf1e261faadb61e468dc0fd7660b39
rosenclosed <git@devrose.de>
2024-12-20 13:44:26 +0100

    adds styles and basic stucture for register screen

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Penny-Kassen-Sim</title>
+    <link rel="stylesheet" href="/res/style/main.min.css">
 </head>
 <body>
-    
+    <div class="main" id="main-div">
+        <div id="register-screen"></div>
+    </div>
 </body>
 </html>
