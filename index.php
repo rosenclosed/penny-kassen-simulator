1e4eabea7e0cbe4df04dd7364d49cc4be3e6399e
rosenclosed <git@devrose.de>
2024-12-20 15:13:16 +0100

    adds basic grid
    adds

--- index.php
+++ index.php
@@ -1,14 +1,109 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Penny-Kassen-Sim</title>
     <link rel="stylesheet" href="/res/style/main.min.css">
 </head>
+
 <body>
     <div class="main" id="main-div">
-        <div id="register-screen"></div>
+        <div class="register-screen" id="register-screen">
+            <div class="screen-left-side">
+                <div class="numpad-area">
+                    <div class="numpad-op-btn-area">
+                        <div class="numpad-back-btn"></div>
+                        <div class="numpad-delete-btn"></div>
+                        <div class="numpad-menge-btn"></div>
+                        <div class="numpad-eingabe-btn"></div>
+                    </div>
+                    <div class="numpad-pad-area">
+                        <div class="numpad-line"></div>
+                        <div class="numpad-pad-btn-area">
+                            <div class="numpad-0-btn"></div>
+                            <div class="numpad-00-btn"></div>
+                            <div class="numpad-1-btn"></div>
+                            <div class="numpad-2-btn"></div>
+                            <div class="numpad-3-btn"></div>
+                            <div class="numpad-4-btn"></div>
+                            <div class="numpad-5-btn"></div>
+                            <div class="numpad-6-btn"></div>
+                            <div class="numpad-7-btn"></div>
+                            <div class="numpad-8-btn"></div>
+                            <div class="numpad-9-btn"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="digital-receipt-area">
+                    <div class="digital-receipt-description-bar"></div>
+                    <div class="digital-receipt-list"></div>
+                    <div class="digital-receipt-bottom-area">
+                        <div class="digital-receipt-sum-area"></div>
+                        <div class="digital-receipt-btn-area">
+                            <div class="digital-receipt-up-btn"></div>
+                            <div class="digital-receipt-down-btn"></div>
+                            <div class="digital-receipt-expand-btn"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="screen-right-side">
+                <div class="operator-btn-area">
+                    <div class="eingabe-btn"></div>
+                    <div class="kiste-btn"></div>
+                    <div class="preisabfrage-btn"></div>
+                    <div class="warenruecknahme-btn"></div>
+                    <div class="summe-btn"></div>
+                    <div class="sum-screen-bar-btn"></div>
+                    <div class="sum-screen-eft-btn"></div>
+                    <div class="zeilenstorno-btn"></div>
+                    <div class="sofortstorno-btn"></div>
+                    <div class="sum-screen-eingabe-btn"></div>
+                    <div class="tragetasche-btn"></div>
+                    <div class="rabatt-btn"></div>
+                    <div class="fundgeld-btn"></div>
+                    <div class="bonvorschub-btn"></div>
+                    <div class="bon-parken-btn"></div>
+                    <div class="code-btn"></div>
+                    <div class="alterpreis-neuerpreis-btn"></div>
+                    <div class="sum-screen-code-btn"></div>
+                    <div class="sum-screen-gutscheine-btn"></div>
+                    <div class="sum-screen-paybackpay-btn"></div>
+                    <div class="sum-screen-stimmtso-btn"></div>
+                    <div class="sum-screen-zeilenstorno-btn"></div>
+                    <div class="sum-screen-sofortstorno-btn"></div>
+                    <div class="sum-screen-5euro-btn"></div>
+                    <div class="sum-screen-10euro-btn"></div>
+                    <div class="sum-screen-20euro-btn"></div>
+                    <div class="sum-screen-50euro-btn"></div>
+                    <div class="nonfood-tax-btn"></div>
+                    <div class="lebensmittel-tax-btn"></div>
+                </div>
+                <div class="modal-btn-area">
+                    <div class="o-g-lose-btn"></div>
+                    <div class="o-g-alle-btn"></div>
+                    <div class="bake-off-btn"></div>
+                    <div class="blumen-btn"></div>
+                    <div class="getraenke-btn"></div>
+                    <div class="kolo-btn"></div>
+                    <div class="home-btn"></div>
+                    <div class="nonfood-btn"></div>
+                </div>
+                <div class="big-info-area">
+                    <div class="big-info-area-line1"></div>
+                    <div class="big-info-area-line2"></div>
+                </div>
+                <div class="small-info-area">
+                    <div class="info-btn"></div>
+                    <div class="payback-indicator"></div>
+                    <div class="time-and-date"></div>
+                    <div class="bediener-no"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
